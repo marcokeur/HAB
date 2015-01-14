8e55543298b2038ff3e1503dac9351f3a202d93c
= <henryfirefox86@gmail.com>
2015-01-14 21:18:43 +0100

    added config.php and updated cartracking.php

---
+++ website/config.php
@@ -0,0 +1,14 @@
+<?php
+/** The name of the database for WordPress */
+define('DB_NAME', 'hab');
+
+/** MySQL database username */
+define('DB_USER', 'hab');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'hab');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+?>
