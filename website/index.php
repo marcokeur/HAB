5c4df020a68472fb8dc400e2bf863af6e7502cbc
Bastiaan Nijkamp <bastiaan@wesnijk.nl>
2015-01-15 19:24:39 +0100

    Updated templates for frontend. Added .gitignore

--- website/index.php
+++ website/index.php
@@ -5,6 +5,23 @@ require_once("inc/lib/smarty/Smarty.class.php");
 require_once("inc/mysqlDb.php");
 
 $smarty = new Smarty;
-$smarty->display('frontend/index.tpl');
+
+switch($_GET['page']) {
+
+	case "about":
+		$smarty->display('frontend/about.tpl');
+	break;
+
+	case "tracker":
+		$smarty->display('frontend/tracker.tpl');
+	break;
+
+	// Display index.
+	default:
+	        $smarty->display('frontend/index.tpl');
+	break;
+
+}
+
 
 ?>
