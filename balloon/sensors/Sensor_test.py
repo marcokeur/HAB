7932a1c54269407688e58291c637b4fbbaaa9c3a
unknown <schimmee@PCL1108.tass.lan>
2015-05-12 19:36:18 +0200

    NTX tranmitter implementation and test

--- balloon/sensors/Sensor_test.py
+++ balloon/sensors/Sensor_test.py
@@ -3,6 +3,7 @@ from i2ctools_python import I2C
 from HTU21D_Humidity import HTU21D
 from MPL3115_Pressure import MPL3115
 from BMP180_Pressure import BMP180
+from NTX2_434_transmitter import NXT2_transmitter
 import time
 
 print "Test"
@@ -13,10 +14,15 @@ print ID
 
 htu = HTU21D()
 pressure = MPL3115()
-pressure2 = BMP180();
+pressure2 = BMP180()
+transmitter = NXT2_transmitter()
 
 while 1:
     print "pressure pascal:  {0}".format(pressure.readpressure())
     print "humidity:  {0}".format(htu.readHumidityData())
     print "pressure2:  {0}".format(pressure2.readpressure())
-    time.sleep(1)
+    transmitter.transmit("$$Hello world,Hello world,Hello world,Hello world,Hello world,Hello world\n")
+    print "have just transmitted text  -Hello world-"
+    time.sleep(2)
+
+transmitter.close()
