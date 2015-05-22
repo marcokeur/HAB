dce073ec7753aa34a4883e7200fc5f79a08ef136
Timo Veldt <timo.veldt@gmail.com>
2015-05-22 10:43:27 +0200

    - Formatted all source files. - Added test to check difference between cases and tests.

--- balloon/camera/imageprocessing/src/main/cpp/library/imageprocessor.hpp
+++ balloon/camera/imageprocessing/src/main/cpp/library/imageprocessor.hpp
@@ -9,27 +9,31 @@
 #endif /* TRACE_LOGGING */
 
 namespace imageprocessor {
-	class Result {
-		public:
-			const bool send;
-			const std::string imageFile;
-			const std::string message;
-			Result(bool send, std::string imageFile, std::string message) : send{send}, imageFile{imageFile}, message{message} {}
+    class Result {
+    public:
+        const bool send;
+        const std::string imageFile;
+        const std::string message;
+
+        Result(bool send, std::string imageFile, std::string message) : send{send}, imageFile{imageFile},
+                                                                        message{message} { }
+
 #ifdef TRACE_LOGGING
 			~Result() {
 				std::cout << "Destroying Result" <<std::endl;
 			}
 #endif /* TRACE_LOGGING */
-	};
+    };
 
-	class Input {
-		public:
-			const std::string imageFile;
-			Input(std::string imageFile) : imageFile{imageFile} {}
-	};
+    class Input {
+    public:
+        const std::string imageFile;
 
-	//const Result* const processImage(const Input* const input);
-        std::unique_ptr<const Result> processImage(const Input* const input);
+        Input(std::string imageFile) : imageFile{imageFile} { }
+    };
+
+    //const Result* const processImage(const Input* const input);
+    std::unique_ptr<const Result> processImage(const Input *const input);
 }
 
 #endif /* IMAGEPROCESSOR_H */
--- balloon/camera/imageprocessing/src/test/cpp/test.cpp
+++ balloon/camera/imageprocessing/src/test/cpp/test.cpp
@@ -4,8 +4,15 @@
 
 using namespace imageprocessor;
 
-TEST(test_images, test_images) {
+TEST(test_correct_file_name, test_images) {
     const Input *const input = new Input("/usr/local/jemoeder.jpg");
+    const std::unique_ptr<const Result> result = imageprocessor::processImage(input);
+
+    ASSERT_EQ(input->imageFile, result->imageFile);
+}
+
+TEST(test_correct_file_name_again, test_images) {
+    const Input *const input = new Input("/usr/local/jemoeder2.jpg");
     const std::unique_ptr<const Result> result = imageprocessor::processImage(input);
 
     ASSERT_EQ(input->imageFile, result->imageFile);
