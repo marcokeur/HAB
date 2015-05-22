04c3b382e738160756332f5a605c93d7944ddb79
Timo Veldt <timo.veldt@gmail.com>
2015-05-22 10:32:43 +0200

    - Formatted all source files.

--- balloon/camera/imageprocessing/src/test/cpp/test.cpp
+++ balloon/camera/imageprocessing/src/test/cpp/test.cpp
@@ -4,16 +4,15 @@
 
 using namespace imageprocessor;
 
-TEST(test_images, test_images)
-{
-	 const Input* const input = new Input("/usr/local/jemoeder.jpg");
+TEST(test_images, test_images) {
+    const Input *const input = new Input("/usr/local/jemoeder.jpg");
     const std::unique_ptr<const Result> result = imageprocessor::processImage(input);
 
-	ASSERT_EQ(input->imageFile, result->imageFile);
+    ASSERT_EQ(input->imageFile, result->imageFile);
 }
 
 
-int main (int argc, char** argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
