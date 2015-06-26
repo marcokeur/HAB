a3cdb23b8b260ad797677c7dfc9dbead57b3506c
Timo Veldt <timo.veldt@gmail.com>
2015-06-26 13:16:42 +0200

    - Created skeleton for image processing module, will listen to 1 message only at the moment. - Added zeromq bindings to imageprocessing main cmake file.

--- balloon/camera/imageprocessing/src/main/main.cpp
+++ balloon/camera/imageprocessing/src/main/main.cpp
@@ -1,5 +1,6 @@
 #include "../imageprocessing/imageprocessor.hpp"
 #include "../logging/logging.hpp"
+#include "zmq.hpp"
 
 using namespace imageprocessor;
 using namespace logging;
@@ -7,12 +8,50 @@ using namespace logging;
 const Logger * const logger = getLogger("main", INFO);
 const Logger * const image_processing_logger = getLogger(IMAGE_PROCESSING_LOGGING_SOURCE, INFO);
 
+#ifndef INCOMING_TOPIC
+#define INCOMING_TOPIC "/camera/picture/raw/location"
+#endif /* INCOMING_TOPIC */
+
+static const std::string IN_TOPIC = INCOMING_TOPIC;
+
+#ifndef OUTGOING_TOPIC
+#define OUTGOING_TOPIC "/camera/picture/processed/location"
+#endif /* OUTGOING_TOPIC */
+
+static const std::string OUT_TOPIC = OUTGOING_TOPIC;
+
 int main(int argc, char **argv) {
-    logger->info("Started main...");
-    const Input *const input = new Input("/usr/local/jemoeder.jpg");
+    logger->info("Started image processing module...");
+
+    logger->info(IN_TOPIC);
+
+    zmq::context_t context(1);
+
+    zmq::socket_t subscriber(context, ZMQ_SUB);
+    subscriber.setsockopt(ZMQ_SUBSCRIBE, IN_TOPIC.data(), IN_TOPIC.size());
+    subscriber.connect("tcp://localhost:5559");
+
+    zmq::socket_t publisher(context, ZMQ_PUB);
+    publisher.connect("tcp://localhost:5560");
+
+    zmq::message_t message(100);
+    subscriber.recv(&message, ZMQ_RCVMORE);
+    subscriber.recv(&message);
+
+    std::string msg_str(static_cast<char*>(message.data()), message.size());
+
+    const Input *const input = new Input(msg_str);
     const std::unique_ptr<const Result> result = imageprocessor::processImage(input);
 
     logger->info(result->message);
 
+    if(result->send) {
+        publisher.send(OUT_TOPIC.data(), OUT_TOPIC.size(), ZMQ_SNDMORE);
+        publisher.send(result->editedImageFile.data(), result->editedImageFile.size(), 0);
+    }
+
+    logger->error("The image processing module is shutting down in a normal way...");
+    context.close();
+    logger->error("The image processing module went down in a normal way...");
     return 0;
 }
