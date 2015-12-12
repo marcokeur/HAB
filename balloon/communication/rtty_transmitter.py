27c6648baee3189fdffa49634c98748683b3a499
Tom Regelink <tom.regelink@gmail.com>
2015-12-12 16:34:25 +0000

    added gps and humidity telemetry

--- balloon/communication/rtty_transmitter.py
+++ balloon/communication/rtty_transmitter.py
@@ -9,7 +9,6 @@ from subscriber import Subscriber
 TELEMETRY_EVERY = 4
 BROKER_URL = "tcp://localhost:5559"
 GOOD_IMAGES_TOPIC = "/camera/picture/processed/location"
-TELEMETRY_TOPIC = "/communication/rf"
 HUMIDITY_TOPIC = "/sensor/humidity"
 GPS_TOPIC = "/sensor/gps/location"
 NTX2_UART = "UART5"
@@ -34,9 +33,9 @@ class RTTY_Transmitter:
 		self.imageSubscriber.start()
 		
 		# Telemetry receiver 
-		self.telemetrySubscriber = Subscriber(BROKER_URL, TELEMETRY_TOPIC)
-		self.telemetrySubscriber.connect()
-		self.telemetrySubscriber.start()
+		self.humiditySubscriber = Subscriber(BROKER_URL, HUMIDITY_TOPIC)
+		self.humiditySubscriber.connect()
+		self.humiditySubscriber.start()
 		
 		# Humidity subscription
 		self.gpsSubscriber = Subscriber(BROKER_URL, GPS_TOPIC)
@@ -61,41 +60,42 @@ class RTTY_Transmitter:
 
 	def send_telemetry(self):
 		'''Get and send telemetry data'''
-		# Get latest telemetry data
-		#data = self.telemetrySubscriber.get()
-		#if data == None:
-		#	return
 		
-		# TODO: build TelemetryPacket from data
-		
-
+		# Get GPS data
 		gps_string = self.gpsSubscriber.get()
+		lat = float(0.0)
+		lon = float(0.0)
+		alt = 0
 		if gps_string != None:
 			gps = gps_string.split(",")
-			print gps
 			lat = float(gps[0])
 			lon = float(gps[1])
 			alt = int(float(gps[2]))
 		
-			# Create TelemetryPacket
-			telemetry = TelemetryPacket(callsign='altran', 
-										sentence_id=self.sentence_id, 
-										lat=lat, 
-										lon=lon, 
-										alt=alt, 
-										in_temp=0.0, 
-										out_temp=0.0, 
-										humidity=0, 
-										air_pressure=100)
+		
+		humidity = self.humiditySubscriber.get()
+		if humidity == None:
+			humidity = 0
 			
-			# Generate telemetry sentence with CRC checksum
-			sentence = telemetry.to_sentence()
-	
-			# Send telemetry packet
-			self.transmitter.transmit(sentence)
-			self.sentence_id += 1
-		else:
-			print "No GPS"
+		
+		# Create TelemetryPacket
+		telemetry = TelemetryPacket(callsign='altran', 
+									sentence_id=self.sentence_id, 
+									lat=lat, 
+									lon=lon, 
+									alt=alt, 
+									in_temp=0.0, 
+									out_temp=0.0, 
+									humidity=humidity, 
+									air_pressure=100)
+		
+		# Generate telemetry sentence with CRC checksum
+		sentence = telemetry.to_sentence()
+
+		# Send telemetry packet
+		self.transmitter.transmit(sentence)
+		self.sentence_id += 1
+
 		
 		
 	def send_image_with_telemetry(self, image_file):
